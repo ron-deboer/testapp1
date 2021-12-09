5a6d37af2a5f24b3cb7d2bc73b4ed8d4e3dff8bf
ron-deboer <ron.deboer@ymail.com>
2021-12-09 22:00:42 +1100

    change color theme

--- src/pages/error404.jsx
+++ src/pages/error404.jsx
@@ -2,7 +2,7 @@ import { Link } from 'solid-app-router';
 
 function Error404(props) {
     return (
-        <div class="no-margin is-full-screen bg-error text-white text-center">
+        <div class="no-margin is-full-screen bg-red text-white text-center">
             <div class="">
                 <h1 class="">ERROR 404</h1>
                 <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
